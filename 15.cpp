c466265afe0a80dc6b525b7637e969799d047f8f
Maliszz <164215388+Maliszz@users.noreply.github.com>
2024-09-10 08:16:21 +0200

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -2,20 +2,11 @@
 using namespace std;
 
 int main(){
-    int a, b, result = 0, aa, c = 1;
+    int a, b, result = 0, c = 1;
     cin >> a >> b;
-
-    aa = a;
-
     while(b){
-        int d = b % 10, w = 1;
-        while(aa){
-            int da = aa % 10;
-            result += da * d * w * c;
-            aa /= 10;
-            w*=10;
-        }
-        aa = a;
+        int d = b % 10;
+        result += a * d * c;
         c*=10;
         b /= 10;
     }
